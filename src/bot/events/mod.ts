23bfce7a071c8c042372adc4d07185f2a2ad1974
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2022-01-26 14:22:36 -0400

    debugging linux errors

--- src/bot/events/mod.ts
+++ src/bot/events/mod.ts
@@ -10,6 +10,7 @@ export const eventData: { events: Partial<EventHandlers>, log: typeof log, } = {
 }
 
 export async function applyEvents() {
+    console.log(__dirname, Deno.cwd(), new URL('.', import.meta.url).pathname)
     for await (const dirEntry of Deno.readDir(__dirname)) {
         if (dirEntry.isFile && dirEntry.name != "mod.ts") {
             log.info(`Loading Event file: ${dirEntry.name}`)
